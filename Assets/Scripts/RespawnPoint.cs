86eb1193c293fe7e0918ba5e0b8b92b80411e188
krasu32 <kacpi1@interia.pl>
2026-06-21 06:31:15 +0200

    Repswn piont issuie

--- Assets/Scripts/RespawnPoint.cs
+++ Assets/Scripts/RespawnPoint.cs
@@ -2,19 +2,9 @@ using UnityEngine;
 
 public class RespawnPoint : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    private void OggerEnter2D(Collider2D _other)
+    private void OnTriggerEnter2D(Collider2D _other)
     {
         GameManager.Instance.platformingRespawnPoint = transform.position;
     }
